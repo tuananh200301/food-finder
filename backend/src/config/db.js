638604c4ba1d3d3fb39975b4dbb6f923ef5a4fdf
anhdt <dtanh200301@gmail.com>
2026-07-15 14:19:53 +0700

    fix sql

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,16 +2,22 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'basejs',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    logging: false, // Set to true to see SQL queries
+    host: process.env.DB_HOST,
+    port: process.env.PORT || 4000,
+    dialect: "mysql",
+    logging: false,
+
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
-
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
